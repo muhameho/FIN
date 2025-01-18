d6a1cb123ad2ff335b298a1f2c80fe5bff4afe21
muhameho <11410120198074@stud.cu.edu.eg>
2025-01-18 15:42:13 +0200

    Add project files.

---
+++ FIN/FIN.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace FIN
+{
+    partial class FIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FIN));
+            this.rtbNames = new System.Windows.Forms.RichTextBox();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pbGithub = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGithub)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbNames
+            // 
+            this.rtbNames.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbNames.Location = new System.Drawing.Point(12, 48);
+            this.rtbNames.Name = "rtbNames";
+            this.rtbNames.ReadOnly = true;
+            this.rtbNames.Size = new System.Drawing.Size(330, 181);
+            this.rtbNames.TabIndex = 2;
+            this.rtbNames.Text = "";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(12, 13);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(330, 29);
+            this.tbPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(38)))), ((int)(((byte)(42)))));
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.FlatAppearance.BorderSize = 0;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("STC Bold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.ForeColor = System.Drawing.Color.White;
+            this.btnBrowse.Location = new System.Drawing.Point(348, 11);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 32);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(38)))), ((int)(((byte)(42)))));
+            this.btnGet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGet.FlatAppearance.BorderSize = 0;
+            this.btnGet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGet.Font = new System.Drawing.Font("STC Bold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.ForeColor = System.Drawing.Color.White;
+            this.btnGet.Location = new System.Drawing.Point(348, 48);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 32);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "Get Names";
+            this.btnGet.UseVisualStyleBackColor = false;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(38)))), ((int)(((byte)(42)))));
+            this.btnExport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExport.FlatAppearance.BorderSize = 0;
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.Font = new System.Drawing.Font("STC Bold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.White;
+            this.btnExport.Location = new System.Drawing.Point(12, 235);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 32);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("STC Bold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(348, 86);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 32);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Start Over";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pbGithub
+            // 
+            this.pbGithub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbGithub.Image = ((System.Drawing.Image)(resources.GetObject("pbGithub.Image")));
+            this.pbGithub.Location = new System.Drawing.Point(404, 248);
+            this.pbGithub.Name = "pbGithub";
+            this.pbGithub.Size = new System.Drawing.Size(31, 31);
+            this.pbGithub.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGithub.TabIndex = 6;
+            this.pbGithub.TabStop = false;
+            this.pbGithub.Click += new System.EventHandler(this.pbGithub_Click);
+            // 
+            // FIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 279);
+            this.Controls.Add(this.pbGithub);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.rtbNames);
+            this.Font = new System.Drawing.Font("STC Regular", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FIN";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FIN";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGithub)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbNames;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pbGithub;
+    }
+}
+
---
+++ FIN/FIN.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace FIN
+{
+    public partial class FIN : Form
+    {
+        public FIN()
+        {
+            InitializeComponent();
+            btnGet.Enabled = false;
+            btnExport.Enabled = false;
+            btnClear.Enabled = false;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tbPath.Text = folderDialog.SelectedPath;
+                    btnGet.Enabled = !string.IsNullOrWhiteSpace(tbPath.Text);
+                    btnClear.Enabled = true;
+                    btnClear.BackColor = Color.FromArgb(255, 128, 128);
+                }
+            }
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(tbPath.Text))
+            {
+                string[] items = Directory.GetFileSystemEntries(tbPath.Text);
+                if (items.Length > 0)
+                {
+                    // Extract just the item names (without the path)
+                    rtbNames.Lines = items.Select(item => Path.GetFileName(item)).ToArray();
+                    btnExport.Enabled = true;
+                    btnClear.Enabled = true;
+                    btnClear.BackColor = Color.FromArgb(255, 128, 128);
+                }
+                else
+                {
+                    rtbNames.Text = "<EMPTY FOLDER>";
+                    btnExport.Enabled = false;
+                    btnClear.Enabled = true;
+                    btnClear.BackColor = Color.FromArgb(255, 128, 128);
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(rtbNames.Text) && rtbNames.Text != "<EMPTY FOLDER>")
+            {
+                string folderName = new DirectoryInfo(tbPath.Text).Name;
+                using (SaveFileDialog saveDialog = new SaveFileDialog())
+                {
+                    saveDialog.Filter = "Text Files (*.txt)|*.txt";
+                    saveDialog.FileName = folderName + "Items.txt";
+
+                    if (saveDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Only write item names to the file (not the full path)
+                        File.WriteAllLines(saveDialog.FileName, rtbNames.Lines);
+                        MessageBox.Show("File Saved Successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        rtbNames.Clear();
+                        tbPath.Clear();
+                        btnGet.Enabled = false;
+                        btnExport.Enabled = false;
+                        btnClear.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbNames.Clear();
+            tbPath.Clear();
+            btnGet.Enabled = false;
+            btnExport.Enabled = false;
+            btnClear.Enabled = false;
+            btnClear.BackColor = Color.FromArgb(255, 192, 192);
+        }
+
+        private void pbGithub_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/muhameho",
+                UseShellExecute = true
+            });
+        }
+    }
+}
